543c6c1bf23d4561c8ef75c2cc0b651ac1d68039
Dayanna Ramirez <alisond.ramirezr@uqvirtual.edu.co>
2026-09-24 14:12:01 -0500

    Creación clase Compra

---
+++ src/Compra.java
@@ -0,0 +1,107 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Compra {
+
+    private int codigoCompra;
+    private LocalDate fecha;
+    private MetodoPago metodoPago;
+    private double valorTotal;
+    private Cliente cliente;
+    private List<DetalleCompra> listaDetalles;
+
+    public Compra(int codigoCompra, LocalDate fecha, MetodoPago metodoPago, Cliente cliente) {
+        this.codigoCompra = codigoCompra;
+        this.fecha = fecha;
+        this.metodoPago = metodoPago;
+        this.cliente = cliente;
+        this.valorTotal = 0.0;
+        this.listaDetalles = new ArrayList<>();
+    }
+
+    public boolean agregarProducto(Producto producto, int cantidad) {
+        if (producto.validarStockDisponible(cantidad)) {
+            DetalleCompra detalle = new DetalleCompra(producto, cantidad);
+            this.listaDetalles.add(detalle);
+            return true;
+        }
+        return false;
+    }
+
+    public double calcularValorTotal() {
+        this.valorTotal = 0.0;
+        for (DetalleCompra detalle : listaDetalles) {
+            this.valorTotal += detalle.getSubtotal();
+        }
+        return this.valorTotal;
+    }
+
+    public void confirmarCompra() {
+        for (DetalleCompra detalle : listaDetalles) {
+            detalle.getProducto().descontarStock(detalle.getCantidadComprada());
+        }
+        calcularValorTotal();
+        cliente.agregarCompra(this);
+    }
+
+    public int getCodigoCompra() {
+        return codigoCompra;
+    }
+
+    public void setCodigoCompra(int codigoCompra) {
+        this.codigoCompra = codigoCompra;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public MetodoPago getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(MetodoPago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<DetalleCompra> getListaDetalles() {
+        return listaDetalles;
+    }
+
+    public void setListaDetalles(List<DetalleCompra> listaDetalles) {
+        this.listaDetalles = listaDetalles;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: " + " | " +
+                "codigoCompra= " + codigoCompra + " | " +
+                "fecha= " + fecha + " | " +
+                "metodoPago= " + metodoPago + " | " +
+                "valorTotal= " + valorTotal + " | " +
+                "cliente= " + cliente.getNombreCompleto() + " | " +
+                "detalles= " + listaDetalles +
+                '}' + " | ";
+    }
+}
